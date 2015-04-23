80efcb4b2f2ab9e938dd4683ad69d02f8885502d
tunv <nguyenvantu.dhbk@gmail.com>
2015-04-24 00:23:57 +0700

    Completed login logout

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test "invalid signup information" do
+   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, user: { name:  "",
@@ -10,5 +10,20 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name:  "Example User",
+                                            email: "user@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }
+    end
+    assert_template 'users/show'
+    # assert_not flash.FILL_IN
+     assert is_logged_in?
   end
 end
